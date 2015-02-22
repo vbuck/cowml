744d036f00c9dab826b83a2ec378b33d4b173a1e
Corey Smith <scorey1609@gmail.com>
2015-02-21 21:19:41 -0800

    pre-parsed html file successfully created using zombie.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var Browser = require('zombie');
+var fs 		= require('fs');
 
-Browser.visit("http://192.168.1.9/tests/basic.html", function(e, browser) {
-	console.log(browser.html())
+function writeFile(html)
+{
+	fs.writeFile(__dirname + '/tests/basic.html', html, function(err) {
+		console.log('It\'s saved!');
+	})
+}
+
+Browser.visit("http://192.168.1.9/tests/basic.cowml.html", function(e, browser) {
+	writeFile(browser.html());
 });
